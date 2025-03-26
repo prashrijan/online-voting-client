dc21bf5d96dd46de1bf4298f6d4189f79c3ceaca
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2025-03-26 16:30:27 +1100

    Created default layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react"
-import { Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+import DefaultLayout from "./components/layout/DefaultLayout"
+import Homepage from "./pages/home/Homepage"
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='*' element={<DefaultLayout />}>
-        
+        <Route index element={<Homepage/>} />
+
         </Route>
       </Routes>
     </>
