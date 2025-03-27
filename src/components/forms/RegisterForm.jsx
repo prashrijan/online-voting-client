4cdac427964c807dd5a48d86d7d720451ecd8996
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2025-03-27 14:50:59 +1100

    Created custom form fields and state management

--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -1,9 +1,79 @@
-import React from "react"
+import React, { useState } from "react"
 
 import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
 
 const RegisterForm = () => {
+  const formInitialValues = {
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+    dob: "",
+    password: "",
+  }
+
+  const [form, setForm] = useState(formInitialValues)
+
+  const formFields = [
+    {
+      label: "Full Name",
+      name: "fullName",
+      type: "text",
+      placeholder: "Enter Full Name",
+      value: form.username,
+      required: true,
+    },
+    {
+      label: "Email",
+      name: "email",
+      type: "email",
+      placeholder: "Enter Email",
+      value: form.email,
+      required: true,
+    },
+    {
+      label: "Phone Number",
+      name: "phoneNumber",
+      type: "number",
+      placeholder: "Enter Phone Number",
+      value: form.phoneNumber,
+      required: true,
+    },
+    {
+      label: "Address",
+      name: "address",
+      type: "text",
+      placeholder: "Enter address",
+      value: form.address,
+      required: true,
+    },
+    {
+      label: "Date of Birth",
+      name: "dob",
+      type: "date",
+      placeholder: "Enter Date of Birth",
+      value: form.dob,
+      required: true,
+    },
+    {
+      label: "Password",
+      name: "password",
+      type: "password",
+      placeholder: "Enter password",
+      value: form.password,
+      required: true,
+    },
+    {
+      label: "Password",
+      name: "password",
+      type: "password",
+      placeholder: "Re-enter password",
+      // value: form.password,
+      required: true,
+    },
+  ]
+
   return (
     <Form className='d-flex flex-column justify-content-center p-5 border'>
       <Form.Group className='mb-3' controlId='formBasicusername'>
@@ -29,3 +99,12 @@ const RegisterForm = () => {
 }
 
 export default RegisterForm
+
+//
+//
+// TODO create custom form fields
+// TODO create initial stage
+// TODO create custom input loop
+// TODO create handle on change
+// TODO create handle on submit
+// TODO create additional form buttons and links to login and to signup using google
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -10,9 +10,9 @@ const Header = () => {
   return (
     <Navbar collapseOnSelect expand='lg' className='bg-body-tertiary'>
       <Container>
-        {/* TODO import correct logo */}
-        {/* <img src="/chunaabFavicon.svg" alt="Chunaab Logo" className="rounded"/> */}
-        <Navbar.Brand href='/'><Logo/></Navbar.Brand>
+        <Navbar.Brand href='/'>
+          <Logo />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <Navbar.Collapse id='responsive-navbar-nav'>
           <Nav className='justify-content-center flex-grow-1'>
