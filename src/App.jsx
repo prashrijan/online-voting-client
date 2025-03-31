5c1190d1dc7b8bc5c91910f6da1ac382961f0f3e
Prashrijan <shresthaprashrijan@gmail.com>
2025-04-01 01:54:31 +1100

    chagnes made by shubik

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import React from "react"
 import { Routes, Route } from "react-router-dom"
 import DefaultLayout from "./components/layout/DefaultLayout"
 import Homepage from "./pages/home/Homepage"
-import Register from "./pages/account/Register"
-import Login from "./pages/account/Login"
-import ForgetPassword from "./pages/account/ForgetPassword"
+import Register from "./pages/auth/Register"
+import Login from "./pages/auth/Login"
+import ForgetPassword from "./pages/auth/ForgetPassword"
 
 function App() {
   return (
     <>
       <Routes>
+        {/* public routes */}
         <Route path='/' element={<DefaultLayout />}>
           <Route index element={<Homepage />} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
           <Route path='/forgetPassword' element={<ForgetPassword />} />
+        </Route>
+        {/* private routes */}
+        <Route>
+            {/* please add private routes (todo) */}
         </Route>
       </Routes>
     </>
