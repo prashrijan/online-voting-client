d9594587338083c131b845b63824f6f5a7e32b19
Prashrijan <shresthaprashrijan@gmail.com>
2025-04-05 10:40:38 +1100

    google auth success page

--- src/pages/auth/GoogleAuthSuccess.jsx
+++ src/pages/auth/GoogleAuthSuccess.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const GoogleAuthSuccess = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const accessToken = urlParams.get("accessToken");
+        const refreshToken = urlParams.get("refreshToken");
+
+        if (accessToken && refreshToken) {
+            localStorage.setItem("accessToken", accessToken);
+            localStorage.setItem("refreshToken", refreshToken);
+        } else {
+            navigate("/login");
+        }
+    });
     return <div>GoogleAuthSuccess</div>;
 };
 
