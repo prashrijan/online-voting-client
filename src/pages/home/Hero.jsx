130070f7fc9265f53bf4bedee9d78d223a0fed76
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2025-04-21 11:27:23 +1000

    UI changes to Hero Section

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -5,26 +5,30 @@ import './homepage.css';
 const Hero = () => {
   return (
     <div className=" hero w-100  d-flex flex-column justify-content-between align-items-center">
-      <h1 className="pt-5 pb-2 "> All-in-one Digital Voting Platform</h1>
+      <h1 className="p-4 fw-bold fs-1 text-center">
+        All-in-one Digital Voting Platform
+      </h1>
 
-      <p className="text-center w-50 ">
+      <p className="text-center w-50 fs-6 ">
         Whether you're organizing a small club election or a nationwide vote,
         Chunaab makes it easy, secure, and stress-free.
       </p>
-      <Button variant="primary" className="getStartedbtn m-4 rounded-5">
+      <Button
+        variant="primary"
+        className="getStartedbtn m-3 px-5 d-flex justify-content-center align-items-center rounded-5 border-3 border-black fs-4 "
+      >
         Get Started
       </Button>
 
-      <div className="joinSection d-flex justify-content-center align-items-center rounded-5">
-        Enter code to join Chunaab
-        <span>
-          <input
-            type="text"
-            className="eventCodeInput mx-3 rounded-3 text-center"
-            placeholder="123 456"
-          />
-        </span>
-        <Button variant="primary" className="px-4 rounded-3">
+      <div className="joinSection m-4 p-3 d-flex justify-content-center align-items-center rounded-5 w-75 flex-wrap  ">
+        <span className=" fs-4 text-center">Enter code to join Live Chunaab</span>
+
+        <input
+          type="text"
+          className="eventCodeInput m-2 rounded-3 text-center fs-5"
+          placeholder="123 456"
+        />
+        <Button variant="primary" className="px-4 m-2 rounded-3 fs-5 ">
           Join
         </Button>
       </div>
