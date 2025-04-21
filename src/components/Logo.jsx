44baf586ba8e4b6d7f44ef5111668dd8b63e557b
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2025-04-21 11:46:47 +1000

    chunaab logo font changed

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo25 from '../assets/logo/chunaab25.svg';
+import '../App.css';
 
 const Logo = () => {
   return (
     <div className="d-flex justify-content-center align-items-center ">
-      <img src={logo25} alt="" className="rounded" />
-      <span>chunaab </span>
+      <img src={logo25} alt="" className="rounded mx-1" />
+      <span className="logo-font fs-4">chunaab </span>
     </div>
   );
 };
