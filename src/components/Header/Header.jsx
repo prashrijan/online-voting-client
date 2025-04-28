f438cb5af502f4cab4325b1887568828fe3f2701
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2025-04-28 23:55:43 +1000

    created Navbar for Guest and Logged in user

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,63 +6,102 @@ import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../Logo';
 import './HeaderStyles.css';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom'; // 🔥 import Link
+
+const GuestNavbar = () => (
+  <Navbar collapseOnSelect expand="lg" className="light-navbar fs-5">
+    <Container>
+      <Navbar.Brand href="/" className="d-flex align-items-center">
+        <Logo darkMode={false} />
+      </Navbar.Brand>
+
+      <Navbar.Toggle
+        aria-controls="responsive-navbar-nav"
+        className="nav-toggle"
+      />
+
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="justify-content-center flex-grow-1 main-nav">
+          <Nav.Link as={Link} to="#" className="nav-link">
+            Live Elections
+          </Nav.Link>
+          <Nav.Link as={Link} to="#" className="nav-link">
+            Features
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about" className="nav-link">
+            About
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact" className="nav-link">
+            Contact
+          </Nav.Link>
+        </Nav>
+
+        <Nav className="auth-buttons">
+          <Nav.Link as={Link} to="/login" className="px-2">
+            <Button variant="outline-primary" className="nav-button">
+              Log In
+            </Button>
+          </Nav.Link>
+          <Nav.Link as={Link} to="/register" className="ps-2">
+            <Button variant="primary" className="nav-button">
+              Register
+            </Button>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
+
+const UserNavbar = () => (
+  <Navbar collapseOnSelect expand="lg" className="light-navbar fs-5">
+    <Container>
+      <Navbar.Brand href="/" className="d-flex align-items-center">
+        <Logo darkMode={false} />
+      </Navbar.Brand>
+
+      <Navbar.Toggle
+        aria-controls="responsive-navbar-nav"
+        className="nav-toggle"
+      />
+
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="justify-content-center flex-grow-1 main-nav">
+          <Nav.Link as={Link} to="#" className="nav-link">
+            Live Elections
+          </Nav.Link>
+          <Nav.Link as={Link} to="#" className="nav-link">
+            My Elections
+          </Nav.Link>
+          <Nav.Link as={Link} to="#" className="nav-link">
+            Results
+          </Nav.Link>
+          <Nav.Link as={Link} to="#" className="nav-link">
+            Help Center
+          </Nav.Link>
+        </Nav>
+
+        <Nav className="auth-buttons">
+          <Nav.Link as={Link} to="#" className="ps-2">
+            <Button variant="danger" className="nav-button">
+              Create Election
+            </Button>
+          </Nav.Link>
+          <Nav.Link as={Link} to="#" className="ps-2">
+            <Button variant="success" className="nav-button">
+              Profile
+            </Button>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
 
 const Header = () => {
   const { user } = useSelector((state) => state.user);
 
-  return (
-    <Navbar collapseOnSelect expand="lg" className="light-navbar">
-      <Container>
-        <Navbar.Brand href="/" className="d-flex align-items-center">
-          <Logo darkMode={false} />
-        </Navbar.Brand>
-
-        <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          className="nav-toggle"
-        />
-
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="justify-content-center flex-grow-1 main-nav">
-            <Nav.Link href="/createElection" className="nav-link">
-              Create Election
-            </Nav.Link>
-            <Nav.Link href="/about" className="nav-link">
-              About
-            </Nav.Link>
-            <Nav.Link href="/contact" className="nav-link">
-              Contact
-            </Nav.Link>
-          </Nav>
-
-          <Nav className="auth-buttons">
-            {user._id ? (
-              <>
-                <Nav.Link href="/logout" className="ps-2">
-                  <Button variant="danger" className="nav-button">
-                    Logout
-                  </Button>
-                </Nav.Link>
-              </>
-            ) : (
-              <>
-                <Nav.Link href="/login" className="px-2">
-                  <Button variant="outline-primary" className="nav-button">
-                    Log In
-                  </Button>
-                </Nav.Link>
-                <Nav.Link href="/register" className="ps-2">
-                  <Button variant="primary" className="nav-button">
-                    Register
-                  </Button>
-                </Nav.Link>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  return user && user._id ? <UserNavbar /> : <GuestNavbar />;
 };
 
 export default Header;
