ec4d5ce980d4cab9eaaefad78df970a4f5957e4c
Prashrijan <shresthaprashrijan@gmail.com>
2025-05-17 15:12:11 +1000

    updated

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginForm from '../../components/Forms/LoginForm';
+import LoginForm from '../../components/Forms/LoginForm.jsx';
 
 const Login = () => {
   return (
