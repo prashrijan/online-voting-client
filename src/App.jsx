18c3dcd4a2f0e0e305bccadfa537bceb25b97afb
Prashrijan <shresthaprashrijan@gmail.com>
2025-05-25 17:17:45 +1000

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,23 @@ import AppRoutes from './routes/AppRoutes';
 import { ToastContainer } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllUserAction } from '@features/user/userAction';
+import { useLocation } from 'react-router-dom';
 
 function App() {
   const activeUsers = useSelector((state) => state.user?.activeUsers);
-
   const dispatch = useDispatch();
+  const location = useLocation();
 
   useEffect(() => {
-    if (activeUsers.length === 0) {
+    const isUserPage = location.pathname.includes('/user');
+    if (isUserPage && activeUsers.length === 0) {
       dispatch(fetchAllUserAction());
     }
-  }, [dispatch, activeUsers.length]);
+  }, [dispatch, activeUsers.length, location.pathname]);
+
   return (
     <>
       <AppRoutes />
-
       <ToastContainer />
     </>
   );
